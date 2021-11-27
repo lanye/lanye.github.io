9af9733012fe818d0dc43f202bb8002f8ee96183
lanye <w@lanyes.org>
2021-11-27 15:05:15 +0800

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-function playmusic(){
+document.oncontextmenu = function() {return false;}
+function playmusic(str){
  var strarr = ['gh','master','net','cdn','https','lanyeaudio','jsdelivr','/',':','mp3','.','lanye'];
  var playbox = document.getElementById('audioBox');
  if(playbox.style.display=='none'){	
+  $(str).removeClass('fa-play').addClass('fa-pause');
   var arr = [];
   for(i=1;i<45;i++){
      arr.push(strarr[4]+strarr[8]+strarr[7]+strarr[7]+strarr[3]+strarr[10]+strarr[6]+strarr[10]+strarr[2]+strarr[7]+strarr[0]+strarr[7]+strarr[11]+strarr[7]+strarr[5]+strarr[7]+i+strarr[10]+strarr[9])
@@ -9,6 +11,8 @@ function playmusic(){
   var myAudio = new Audio(); 
   myAudio.preload = true; 
   myAudio.controls = true; 
+  myAudio.controlsList = 'nodownload';
+  myAudio.oncontextmenu = 'return false';
   myAudio.src = arr.pop();  
   myAudio.addEventListener('ended', playEndedHandler, false); 
   myAudio.play(); 
@@ -17,11 +21,12 @@ function playmusic(){
   playbox.style.display='block';
   function playEndedHandler(){ 
      myAudio.src = arr.pop(); 
-     myAudio.play();    
+     myAudio.play();
     !arr.length && myAudio.removeEventListener('ended',playEndedHandler,false);
   } 
 }else{
- playbox.style.display='none';
- playbox.innerHTML='';
+  $(str).removeClass('fa-pause').addClass('fa-play');
+  playbox.style.display='none';
+  playbox.innerHTML='';
 }
 }
