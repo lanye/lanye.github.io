b2186b14457ad5ede5a4070f887ee4cdb9e0f931
lanye <w@lanyes.org>
2022-06-03 12:55:58 +0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 document.oncontextmenu = function() {return false;}
 function playmusic(str){
- var strarr = ['gh','master','org','raw','https','lanyeaudio','fastgit','/',':','mp3','.','lanye'];
+ var strarr = ['gh','master','net','cdn','https','lanyeaudio','jsdelivr','/',':','mp3','.','lanye'];
  var playbox = document.getElementById('audioBox');
  if(playbox.style.display=='none'){	
   $(str).removeClass('fa-play').addClass('fa-pause');
@@ -11,7 +11,7 @@ function playmusic(str){
   }
   var arr = [];
   for(i=1;i<50;i++){
-     arr.push(strarr[4]+strarr[8]+strarr[7]+strarr[7]+strarr[3]+strarr[10]+strarr[6]+strarr[10]+strarr[2]+strarr[7]+strarr[11]+strarr[7]+strarr[5]+strarr[7]+strarr[1]+strarr[7]+i+strarr[10]+strarr[9])
+     arr.push(strarr[4]+strarr[8]+strarr[7]+strarr[7]+strarr[3]+strarr[10]+strarr[6]+strarr[10]+strarr[2]+strarr[7]+strarr[0]+strarr[7]+strarr[11]+strarr[7]+strarr[5]+strarr[7]+i+strarr[10]+strarr[9])
   }
   arr.push('https://djqcdn.djshow.cn/djs/64/c8/0c4ccd37973355f5_l.m4a');
   var myAudio = new Audio(); 
